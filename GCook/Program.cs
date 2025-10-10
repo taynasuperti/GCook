07432f25a3934753dc7c49dad578d05ed5454c5b
taynasuperti <taynasuperti@gmail.com>
2025-10-10 20:35:19 -0300

    criado o serviço de conexão

--- GCook/Program.cs
+++ GCook/Program.cs
@@ -1,9 +1,40 @@
+using GCook.Data;
+using GCook.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Serviço de conexão com o banco de dados - mysql
+string conexao = builder.Configuration.GetConnectionString("GCook")
+var versao = ServerVersion.AutoDetect(conexao);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(conexao, versao)
+);
+
+//Serviço de identificação de usuário
+builder.Service.AddIdentity<Usuario, IdentityRole>(
+    options =>
+    {
+        options.SignIn.RequiredConfirmedEmail = true;
+        options.User.RequiredUniqueEmail = true;
+    }
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaulTokenProviders();
+
+
 var app = builder.Build();
+
+//Garantir que o banco exista ao executar o projeto
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+    await db.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,6 +47,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
