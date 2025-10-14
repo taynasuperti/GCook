9e66b787e0e69f5ce728d90b39e47c8edaa65a44
Tayná <taynasuperti@gmail.com>
2025-10-13 23:41:26 -0300

    criada as viewmodels - homevm e loginvm

--- GCook/Models/Receita.cs
+++ GCook/Models/Receita.cs
@@ -1,46 +1,43 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 namespace GCook.Models;
 
 [Table("Receita")]
-    public class Receita
-    {
-        [Key]
-        public int Id { get; set; }
+public class Receita
+{
+    [Key]
+    public int Id { get; set; }
 
-        //adicionar a fk da categoria
-        [Display(Name = "Categoria")]
-        [Required(ErrorMessage = "A Categoria é obrigatória")]
-        public int CategoriaId { get; set; }
-        [ForeignKey("CategoriaId")]
-        [Display(Name = "Categoria")]
+    [Display(Name = "Categoria")]
+    [Required(ErrorMessage = "A Categoria é obrigatória")]
+    public int CategoriaId { get; set; }
+    [ForeignKey("CategoriaId")]
+    [Display(Name = "Categoria")]
+    public Categoria Categoria { get; set; }
 
-        public Categoria Categoria { get; set; }
+    [StringLength(100)]
+    [Required(ErrorMessage = "O Nome é obrigatório")]
+    public string Nome { get; set; }
 
-        [StringLength(100)]
-        [Required(ErrorMessage = "O Nome da Receita é obrigatório")]
-        public string Nome { get; set; }
+    [StringLength(1000)]
+    [Display(Name = "Descrição")]
+    public string Descricao { get; set; }
 
-        [StringLength(1000)]
-        [Required(ErrorMessage = "A Descrição da Receita é obrigatória")]
-        public string Descricao { get; set; }
+    [StringLength(30)]
+    [Display(Name = "Tempo de Preparo")]
+    public string TempoPreparo { get; set; }
 
-        [StringLength(30)]
-        [Required(ErrorMessage = "O Tempo de Preparo da Receita é obrigatório")]
-        public string TempoPreparo { get; set; }
+    public int Rendimento { get; set; }
 
-        //não tenho certeza se assim tá certo
-        public int Rendimento { get; set; }
+    public Dificuldade Dificuldade { get; set; }
 
-        public Dificuldade Dificuldade { get; set; }
+    [StringLength(300)]
+    public string Foto { get; set; }
 
-        //adicionar a fk da dificuldade
+    [StringLength(8000)]
+    [Display(Name = "Modo de Preparo")]
+    [Required(ErrorMessage = "O Modo de Preparo é obrigatório")]
+    public string Preparo { get; set; }
 
-        [StringLength(300)]
-        public string Foto { get; set; }
-
-        [StringLength(8000)]
-        [Required(ErrorMessage = "O Modo de Preparo da receita é obrigatório")]
-        public string Preparo { get; set; }
-    }
+    public List<ReceitaIngrediente> Ingredientes { get; set; }
+}
--- GCook/Models/Usuario.cs
+++ GCook/Models/Usuario.cs
@@ -1,17 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
-
 namespace GCook.Models;
+
 [Table("Usuario")]
-    public class Usuario
-    {
-        [Required]
-        [StringLength(50)]
-        public string Nome { get; set; }
-        [DataType(DataType.Date)]
+public class Usuario : IdentityUser
+{
+    [Required]
+    [StringLength(50)]
+    public string Nome { get; set; }
 
-        public DateTime? DataNascimento { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime? DataNascimento { get; set; }
 
-        [StringLength(30)]
-        public string Foto { get; set; }
-    }
+    [StringLength(300)]
+    public string Foto { get; set; }
+}
